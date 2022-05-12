338a6402b608e3c3c9d1473527001d3432c1e2c5
danasone <57491208+danil31219as@users.noreply.github.com>
2022-05-13 01:39:15 +0400

    Update beit.py

--- models/beit.py
+++ models/beit.py
@@ -248,13 +248,13 @@ class VisionTransformer(nn.Module):
         else:
             self.rel_pos_bias = None
 
-        dpr = [x.item() for x in torch.linspace(0, 0.1, config.transformer.num_layers)]  # stochastic depth decay rule
+        dpr = [x.item() for x in torch.linspace(0, config.transformer.dropout_rate, config.transformer.num_layers)]  # stochastic depth decay rule
         self.use_rel_pos_bias = False
         self.blocks = nn.ModuleList([
             Block(
                 dim=self.embed_dim, num_heads=config.transformer.num_heads, mlp_ratio=config.transformer.mlp_ratio, qkv_bias=config.transformer.qkv_bias, qk_scale=None,
                 drop=config.transformer.dropout_rate, attn_drop=config.transformer.attention_dropout_rate, drop_path=dpr[i], norm_layer=norm_layer,
-                init_values=None, window_size=self.patch_embed.patch_shape)
+                init_values=config.transformer.init_value, window_size=self.patch_embed.patch_shape)
             for i in range(config.transformer.num_layers)])
         self.norm = nn.Identity()
         self.fc_norm = norm_layer(self.embed_dim)
